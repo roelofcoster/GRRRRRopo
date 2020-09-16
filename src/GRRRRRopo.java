378e7ae9681ec086c4b2551d04f48c140244214f
roelofcoster <roelofcoster@gmail.com>
2020-09-16 14:49:05 +0200

    Initiële commit

---
+++ src/GRRRRRopo.java
@@ -0,0 +1,9 @@
+import java.net.Authenticator;
+import java.net.http.HttpClient;
+
+public class GRRRRRopo {
+	public static void main(String[] args){
+		System.out.println(Authenticator.getDefault());
+		HttpClient client;
+	}
+}
