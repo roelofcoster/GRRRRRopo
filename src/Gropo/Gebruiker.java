cb913fc3775e9dc65fa841bc02a749d82b0f2b19
roelofcoster <roelofcoster@gmail.com>
2020-09-16 16:26:42 +0200

    Add files via upload

---
+++ src/Gropo/Gebruiker.java
@@ -0,0 +1,8 @@
+package Gropo;
+public class Gebruiker {
+	private Gebruiker(){
+		throw new IllegalAccessError("Wegwezen!");
+	};
+	public static final String gebruiker = "gebruiker";
+	public static final String wachtwoord = "qwertyuiop[]\\";
+}
