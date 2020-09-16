214f86e47c0032c51b04e57e11a6aaae54dbca37
roelofcoster <roelofcoster@gmail.com>
2020-09-16 15:02:09 +0200

    HttpClient doet het voor het eerst

--- src/GRRRRRopo.java
+++ src/GRRRRRopo.java
@@ -1,10 +1,18 @@
-import java.net.Authenticator;
+//import java.net.Authenticator;
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class GRRRRRopo {
 	public static void main(String[] args){
-		System.out.println(Authenticator.getDefault());
-		HttpClient client;
-		System.out.println("Blaat");
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+				.uri(URI.create("http://openjdk.java.net/"))
+				.build();
+		client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+				.thenApply(HttpResponse::body)
+				.thenAccept(System.out::println)
+				.join();
 	}
 }
