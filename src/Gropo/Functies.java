6e729ad4ff0cc8ceb3b01bcd414dcfbb913e9fa4
roelofcoster <roelofcoster@gmail.com>
2020-09-19 20:32:56 +0200

    Hij logt in

--- src/Gropo/Functies.java
+++ src/Gropo/Functies.java
@@ -3,17 +3,25 @@ package Gropo;
 import java.net.URI;
 import java.net.http.HttpRequest;
 
-public class Bericht {
-	private Bericht(){
+public class Functies {
+	private Functies(){
 		throw new IllegalAccessError("Kssssst!");
 	}
 
-	public static HttpRequest postBericht(URI uri, String bericht) {
+	public static HttpRequest postMaar(URI uri, String bericht) {
 		HttpRequest uitkomst =
 				HttpRequest.newBuilder()
 						.uri(uri)
 						.setHeader("Content-Type", "application/x-www-form-urlencoded")
 						.POST(HttpRequest.BodyPublishers.ofString(bericht))
+						.build();
+		return uitkomst;
+	}
+
+	public static HttpRequest getMaar(URI uri){
+		HttpRequest uitkomst =
+				HttpRequest.newBuilder()
+						.uri(uri)
 						.build();
 		return uitkomst;
 	}
--- src/Gropo/GRRRRRopo.java
+++ src/Gropo/GRRRRRopo.java
@@ -1,31 +1,49 @@
 package Gropo;
 
 import java.io.IOException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.*;
+
 
 public class GRRRRRopo {
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String gebruiker = Gebruiker.gebruiker;
-		String wachtwoord = Gebruiker.wachtwoord;
-		String bericht = "email=" + gebruiker +
-				"&password=" + wachtwoord;
-		URI uri = URI.create("https://lets.gropo.nl/auth/login");
+		URI uri = URI.create(args[0]);
+		String bericht = "email=" + args[1] +
+				"&password=" + args[2];
 
+		CookieHandler.setDefault(new CookieManager());
 		HttpClient client = HttpClient.newBuilder()
 				.version(HttpClient.Version.HTTP_1_1)
+				.cookieHandler(CookieHandler.getDefault())
+				.connectTimeout(Duration.ofSeconds(30))
+				.followRedirects(HttpClient.Redirect.NEVER)
 				.build();
-		HttpRequest request = Bericht.postBericht(
+		HttpRequest request = Functies.postMaar(
 				uri,
 				bericht);
 
-		String uitk = client.send(
+		client.send(
 				request,
 				HttpResponse.BodyHandlers.ofString())
 			.body();
+
+		String uitk = client.send(
+				Functies.getMaar(URI.create("https://lets.gropo.nl/booking")),
+				HttpResponse.BodyHandlers.ofString()
+		).body();
+
 		System.out.println(uitk);
+
+
 	}
 
 }
